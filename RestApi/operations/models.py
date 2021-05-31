088450be74102daea9cab8e19d76cccabbdde9e6
Kévin DANG <kevin.dang01@etu.univ-paris-diderot.fr>
2021-06-01 00:08:35 +0200

    Add operations model

---
+++ RestApi/operations/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Operation(models.Model):
+    titreoperation = models.CharField(max_length=255, blank=False, default='')
+    entreprise = models.CharField(max_length=255, blank=False, default='')
+    annee_de_livraison = models.CharField(max_length=255, blank=False, default='')
+    ville = models.CharField(max_length=255, blank=False, default='')
+    ppi = models.CharField(max_length=255, blank=False, default='')
+    lycee = models.CharField(max_length=255, blank=False, default='')
+    notification_du_marche = models.CharField(max_length=255, blank=False, default='')
+    codeuai = models.CharField(max_length=255, blank=False, default='')
+    longitude = models.DecimalField(max_digits=None, decimal_places=None)
+    etat_d_avancement = models.CharField(max_length=255, blank=False, default='')
+    montant_des_ap_votes_en_meu = models.IntegerField()
+    cao_attribution = models.CharField(max_length=255, blank=False, default='')
+    latitude = models.DecimalField(max_digits=None, decimal_places=None)
+    maitrise_d_oeuvre = models.CharField(max_length=255, blank=False, default='')
+    mode_de_devolution = models.CharField(max_length=255, blank=False, default='')
+    annee_d_individualisation = models.CharField(max_length=255, blank=False, default='')
+    enveloppe_prev_en_meu = models.DecimalField(max_digits=None, decimal_places=None)

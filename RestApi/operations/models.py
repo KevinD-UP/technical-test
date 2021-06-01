43eb5c2fc8014189a284101657a95e56f32e7878
Kévin DANG <kevin.dang01@etu.univ-paris-diderot.fr>
2021-06-02 00:40:24 +0200

    correction database

--- RestApi/operations/models.py
+++ RestApi/operations/models.py
@@ -3,4 +3,20 @@ from django.db import models
 
 # Create your models here.
 class Operation(models.Model):
-    data = models.JSONField(default=dict)
+    titreoperation = models.CharField(max_length=255, blank=False, default='')
+    entreprise = models.CharField(max_length=255, blank=False, default='')
+    annee_de_livraison = models.CharField(max_length=255, blank=False, default='')
+    ville = models.CharField(max_length=255, blank=False, default='')
+    ppi = models.CharField(max_length=255, blank=False, default='')
+    lycee = models.CharField(max_length=255, blank=False, default='')
+    notification_du_marche = models.CharField(max_length=255, blank=False, default='')
+    codeuai = models.CharField(max_length=255, blank=False, default='')
+    longitude = models.DecimalField(max_digits=100, decimal_places=90, default=0.0)
+    etat_d_avancement = models.CharField(max_length=255, blank=False, default='')
+    montant_des_ap_votes_en_meu = models.IntegerField(default=0)
+    cao_attribution = models.CharField(max_length=255, blank=False, default='')
+    latitude = models.DecimalField(max_digits=100, decimal_places=90, default=0.0)
+    maitrise_d_oeuvre = models.CharField(max_length=255, blank=False, default='')
+    mode_de_devolution = models.CharField(max_length=255, blank=False, default='')
+    annee_d_individualisation = models.CharField(max_length=255, blank=False, default='')
+    enveloppe_prev_en_meu = models.DecimalField(max_digits=100, decimal_places=90, default=0.0)
--- RestApi/operations/serializers.py
+++ RestApi/operations/serializers.py
@@ -6,4 +6,21 @@ class OperationSerializer(serializers.ModelSerializer):
  
     class Meta:
         model = Operation
-        fields =  ('id', 'data')
+        fields =  ('id',
+                'titreoperation',
+                'entreprise',
+                'annee_de_livraison',
+                'ville',
+                'ppi',
+                'lycee',
+                'notification_du_marche',
+                'codeuai',
+                'longitude',
+                'etat_d_avancement',
+                'montant_des_ap_votes_en_meu',
+                'cao_attribution',
+                'latitude',
+                'maitrise_d_oeuvre',
+                'mode_de_devolution',
+                'annee_d_individualisation',
+                'enveloppe_prev_en_meu')

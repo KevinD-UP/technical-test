eaa36fc34ebe2b404a3544b06bd5f0d30e7cb64f
Kévin DANG <kevin.dang01@etu.univ-paris-diderot.fr>
2021-06-01 23:50:02 +0200

    Modification datatype

--- RestApi/operations/urls.py
+++ RestApi/operations/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url 
-from tutorials import views
+from operations import views
 
 urlpatterns = [
-    url(r'^api/operations$', view.operation_list),
+    url(r'^api/operations$', views.operation_list),
     url(r'^api/operations/(?P<pk>[0-9]+)$', views.operation_detail)
 ]
